b8368728599dc481ab83174ff39485449cc16347
Rakesh Raj <bsrakesh@pdmrindia.com>
2023-05-22 18:59:32 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var editor = new Quill('.editor');  // First matching element will be used
+
+var container = document.getElementById('editor');
+var editor = new Quill(container);
+
+var container = $('.editor').get(0);
+var editor = new Quill(container);
+
+
+var options = {
+    debug: 'info',
+    modules: {
+      toolbar: '#toolbar'
+    },
+    placeholder: 'Compose an epic...',
+    readOnly: true,
+    theme: 'snow'
+  };
+  var editor = new Quill('#editor', options);
